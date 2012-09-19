7e1850e58381faa0bc77a587b4d59ac2313f7aa0
Reilly Watson <reillywatson@gmail.com>
2012-09-19 00:15:49 -0400

    Fix off by one error

--- 97.clj
+++ 97.clj
@@ -1,4 +1,4 @@
-(defn pascal [n]
+(fn pascal [row]
   (letfn [(factorial [x] (if (zero? x) 1 (* x (factorial (- x 1)))))
 			(nchooser [n r] (/ (factorial n) (* (factorial r) (factorial (- n r)))))]
-	(map #(nchooser n %) (range (inc n)))))
+	(map #(nchooser (dec row) %) (range row))))
