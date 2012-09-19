dd155010c1e04ef9ae7242e2689c22213cd84662
Reilly Watson <reillywatson@gmail.com>
2012-09-18 23:07:20 -0400

    Remove debugging statement

--- 100.clj
+++ 100.clj
@@ -2,7 +2,6 @@
 	(letfn [(gcd [& rest]
 		(let [smallest (apply min rest) largest (apply max rest)]
 			(if (= smallest largest)
-				(do (println "largest:" largest)
-				largest)
+				largest
 				(apply gcd (map #(if (= % smallest) % (- % smallest)) rest)))))]
 	(reduce (fn [x y] (/ (* x y) (gcd x y))) args)))
